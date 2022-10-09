3e38b813e4cbaea9db60e0cd05ef77833679c093
Valentin Denisenkov <vdnsnkv@gmail.com>
2022-10-09 20:00:05 +0600

    commit not meaningful files

---
+++ py_lib/kafka/producer.py
@@ -0,0 +1,62 @@
+import logging
+from threading import Thread
+
+from confluent_kafka import Producer
+
+PRODUCER_POLL_TIMEOUT = 0.1
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+class KafkaProducer(object):
+    def __init__(self, brokers: str, client_id: str, params: dict = None):
+        if params is None:
+            params = {}
+
+        self._producer = Producer(
+            {
+                "bootstrap.servers": brokers,
+                "client.id": client_id,
+                "logger": logger,
+                **params,
+            }
+        )
+        self._is_polling = False
+        self._poll_thread = Thread(target=self._run_poll_loop)
+        return
+
+    def _run_poll_loop(self):
+        logger.info("running producer poll loop")
+        while self._is_polling:
+            self._producer.poll(PRODUCER_POLL_TIMEOUT)
+        return
+
+    def start(self):
+        logger.info("starting producer poll loop")
+        self._is_polling = True
+        self._poll_thread.start()
+        return
+
+    def stop(self):
+        logger.info("stopping producer poll loop")
+        self._is_polling = False
+        self._poll_thread.join()
+        return
+
+    @staticmethod
+    def _on_delivery(err, msg):
+        if err is not None:
+            logger.error(f"failed to deliver message: {msg}: {err}")
+        else:
+            logger.debug(f"message produced: {msg}")
+
+    def produce(self, topic: str, value: str, key: str = None):
+        self._producer.produce(
+            topic, key=key, value=value, on_delivery=self._on_delivery
+        )
+
+        # if poll thread is not started, make poll
+        if not self._is_polling:
+            self._producer.poll(0)
+        return
